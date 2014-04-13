bef9036a0c95443d5c9ec90b73be2aa28d086aee
Chris Aubuchon <aubuchcl@gmail.com>
2014-04-13 05:29:55 -0700

    Forgot to comment out reflection.  Update is with commented reflection.

--- week_5/6_validate_credit_card/my_solution.rb
+++ week_5/6_validate_credit_card/my_solution.rb
@@ -116,24 +116,24 @@ puts bad_num
 # 5. Reflection 
 
 
-What parts of your strategy worked? What problems did you face?
-	My strategy was to manipulate the data with public methods.  Order of operations(if you will) of what you can do to strings, integers, and arrays was the main problem
-What questions did you have while coding? What resources did you find to help you answer them?
-	I was in irb the entire time.  I had lots of questions but I just kept trying different solutions until the code worked
-What concepts are you having trouble with, or did you just figure something out? If so, what?
-	I figured out that irb isn't so scary.
-Did you learn any new skills or tricks?
-	I learned that I can call about 50 methods in a row and as long as they are all viable to call on the data type and in the correct sequence its all good
-How confident are you with each of the learning objectives?
-	I feel like I had an idea about how I wanted to solve this problem(after reading through what was necessary) and it worked.  That helps my confidence loads.
-Which parts of the challenge did you enjoy?
-	Manipulating the data as much as I wanted.
-Which parts of the challenge did you find tedious?
-	none.
+# What parts of your strategy worked? What problems did you face?
+# 	My strategy was to manipulate the data with public methods.  Order of operations(if you will) of what you can do to strings, integers, and arrays was the main problem
+# What questions did you have while coding? What resources did you find to help you answer them?
+# 	I was in irb the entire time.  I had lots of questions but I just kept trying different solutions until the code worked
+# What concepts are you having trouble with, or did you just figure something out? If so, what?
+# 	I figured out that irb isn't so scary.
+# Did you learn any new skills or tricks?
+# 	I learned that I can call about 50 methods in a row and as long as they are all viable to call on the data type and in the correct sequence its all good
+# How confident are you with each of the learning objectives?
+# 	I feel like I had an idea about how I wanted to solve this problem(after reading through what was necessary) and it worked.  That helps my confidence loads.
+# Which parts of the challenge did you enjoy?
+# 	Manipulating the data as much as I wanted.
+# Which parts of the challenge did you find tedious?
+# 	none.
 
-I saved a document of my irb for the night.  It was around 4700 lines when I copied it to a text doc.  
-I did almost all of my work in irb for questions 4-6.  I am really excited about progressing with this tool
-and I feel like I am starting to understand that irb is awesome.  
+# I saved a document of my irb for the night.  It was around 4700 lines when I copied it to a text doc.  
+# I did almost all of my work in irb for questions 4-6.  I am really excited about progressing with this tool
+# and I feel like I am starting to understand that irb is awesome.  
 
 
 
